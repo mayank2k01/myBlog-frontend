8d725ccf82daf148c109229f1dae84fd540d828e
mayank2k01 <jhamayank707@gmail.com>
2024-07-06 01:54:36 +0530

    lat

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import {Navigate} from "react-router-dom";
 
 const RegisterPage = () => {
   const [username,setUsername] = useState('');
   const [password,setPassword] = useState('');
+  const [redirect,setRedirect] = useState(false);
 
   async function register(ev){
     ev.preventDefault();
@@ -12,6 +14,11 @@ const RegisterPage = () => {
       headers: {'Content-Type':'application/json'},
     })
     if(response.ok==false) alert('registration failed')
+    else setRedirect(true);
+  }
+
+  if(redirect){
+    return <Navigate to={'/login'}/>
   }
 
   return (
